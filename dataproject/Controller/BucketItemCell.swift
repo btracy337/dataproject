11f7ff13223eaa383c4303fdb5aabd2484ed8943
Bryan Tracy <btracy337@gmail.com>
2018-11-30 08:28:16 -0700

    Updated project: See Descpription
    Changed files in the controller to be Cocoa Touch class.
    
    Added constraints in the storyboard.
    
    Added more bugs to fix later.

--- dataproject/Controller/BucketItemCell.swift
+++ dataproject/Controller/BucketItemCell.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
